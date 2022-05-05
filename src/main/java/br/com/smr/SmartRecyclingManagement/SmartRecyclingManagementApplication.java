56b073394316d7a802e66977632ba137b182a1cc
Andre0Luis <andre_luis.t@hotmail.com>
2022-05-04 21:02:18 -0300

    [ALT] Primeiro commit

---
+++ src/main/java/br/com/smr/SmartRecyclingManagement/SmartRecyclingManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.smr.SmartRecyclingManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartRecyclingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartRecyclingManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/smr/SmartRecyclingManagement/SmartRecyclingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.smr.SmartRecyclingManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartRecyclingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
